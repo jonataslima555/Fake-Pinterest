006f9caa35ebcaaed6f19949c5b761bb720d55f3
Jonatas Lima <desista0404@gmail.com>
2024-08-26 19:35:24 -0300

    .env secure

---
+++ __init__.py
@@ -0,0 +1,18 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+DB_PASSWORD = getenv('DB_PASSWORD')
+
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+
+APP = Flask(__name__)
+APP.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///data.db'
+APP.config["SECRET_KEY"] = DB_PASSWORD
+
+print(DB_PASSWORD)
